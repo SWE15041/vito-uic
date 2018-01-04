4e0a58c5cca0bd97f00c87cdec3f457122c0f0ad
zhaixm <zxmsdyz@sina.com>
2018-01-04 23:52:57 +0800

    webconfig中加入fastjson相关配置

--- server/src/main/java/com/vito/uic/core/config/WebConfig.java
+++ server/src/main/java/com/vito/uic/core/config/WebConfig.java
@@ -1,11 +1,14 @@
 package com.vito.uic.core.config;
 
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import com.vito.uic.web.AppInitListener;
 import com.vito.uic.web.UICAuthFilter;
+import com.vito.website.core.config.MyFastJsonHttpMessageConverter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -14,7 +17,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Configuration
-public class WebConfiguration {
+public class WebConfig {
 
     @Bean
     public ServletListenerRegistrationBean<AppInitListener> getAppInitListener() {
@@ -36,6 +39,11 @@ public class WebConfiguration {
     }
 
     @Bean
+    public FastJsonHttpMessageConverter fastJsonHttpMessageConverter() {
+        return new MyFastJsonHttpMessageConverter();
+    }
+
+    @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurerAdapter() {
             @Override
@@ -44,6 +52,12 @@ public class WebConfiguration {
                         .allowedHeaders("*")
                         .allowedMethods("*")
                         .allowedOrigins("*");
+            }
+
+            @Override
+            public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+                converters.add(fastJsonHttpMessageConverter());
+                super.configureMessageConverters(converters);
             }
         };
     }
--- server/src/main/java/com/vito/website/web/listener/SystemInitListener.java
+++ server/src/main/java/com/vito/website/web/listener/SystemInitListener.java
@@ -1,7 +1,5 @@
 package com.vito.website.web.listener;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.vito.website.core.Application;
 import com.vito.website.core.cache.SpringBeanContext;
 import com.vito.website.core.cache.SystemDataHolder;
@@ -37,14 +35,6 @@ public class SystemInitListener implements ServletContextListener {
         SystemDataHolder.init();
         SystemDataHolder.setParam(SystemDataHolder.WEBCONTEXT_REAL_PATH, event.getServletContext().getRealPath("/"));
         SystemDataHolder.setParam(SystemDataHolder.WEBCONTEXT_PATH, event.getServletContext().getContextPath());
-        // 默认的日期转换格式
-        JSON.DEFFAULT_DATE_FORMAT = "yyyy-MM-dd";
-        // 关闭fastjson对循环引用的支持。 如果开启该功能fastjson会使用$ref来链接同一对象，导致其他地方无法正常获取需要的值 -->
-        JSON.DEFAULT_GENERATE_FEATURE |= SerializerFeature.DisableCircularReferenceDetect.getMask();
-        // 如果实体属性为enum类型，转换为json时使用enum ordinal而不使用enum name
-        JSON.DEFAULT_GENERATE_FEATURE = SerializerFeature.config(JSON.DEFAULT_GENERATE_FEATURE, SerializerFeature.WriteEnumUsingName, false);
-        // 转换为json时将对象中null值也正常输出
-        JSON.DEFAULT_GENERATE_FEATURE = SerializerFeature.config(JSON.DEFAULT_GENERATE_FEATURE, SerializerFeature.WriteMapNullValue, true);
     }
 
 }
