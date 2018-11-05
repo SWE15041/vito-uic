d169a549d71e19d2a5232cd72c0805c3c78fb9e7
zhaixm <zxmsdyz@sina.com>
2018-11-05 09:44:32 +0800

    uic模块服务配置类完善

--- service/src/main/java/com/jay/vito/uic/core/AutoConfiguration.java
+++ service/src/main/java/com/jay/vito/uic/core/AutoConfiguration.java
@@ -7,10 +7,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
-@EnableJpaRepositories(basePackages = {"com.jay.vito"}, repositoryBaseClass = MyJpaRepositoryImpl.class, repositoryFactoryBeanClass = MyRepositoryFactoryBean.class)
-@ComponentScan({"com.jay.vito.uic"})
+@EnableJpaRepositories(basePackages = {"com.jay.vito.uic.domain"}, repositoryBaseClass = MyJpaRepositoryImpl.class, repositoryFactoryBeanClass = MyRepositoryFactoryBean.class)
+@ComponentScan({"com.jay.vito.uic.service", "com.jay.vito.uic.web"})
 public class AutoConfiguration {
-
 
 
 }
