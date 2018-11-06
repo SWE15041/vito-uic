925b8e90b19e2c1562dc4d5386845aa1803598d2
zhaixm <zxmsdyz@sina.com>
2018-11-06 23:21:19 +0800

    使用vitoWeb自动配置注解

--- service/src/main/java/com/jay/vito/uic/core/AutoConfiguration.java
+++ service/src/main/java/com/jay/vito/uic/core/AutoConfiguration.java
@@ -3,6 +3,7 @@ package com.jay.vito.uic.core;
 import com.jay.vito.storage.core.MyJpaRepositoryImpl;
 import com.jay.vito.storage.core.MyRepositoryFactoryBean;
 import com.jay.vito.storage.domain.MybatisMapper;
+import com.jay.vito.website.core.config.EnableVitoWebServer;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
@@ -13,7 +14,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories(basePackages = {"com.jay.vito.uic.domain"}, repositoryBaseClass = MyJpaRepositoryImpl.class, repositoryFactoryBeanClass = MyRepositoryFactoryBean.class)
 @EntityScan("com.jay.vito.uic.domain")
 @MapperScan(basePackages = "com.jay.vito.uic.domain", markerInterface = MybatisMapper.class)
-@ComponentScan({"com.jay.vito.storage", "com.jay.vito.uic.service", "com.jay.vito.uic.web"})
+@ComponentScan({"com.jay.vito.uic.service", "com.jay.vito.uic.web"})
+@EnableVitoWebServer
 public class AutoConfiguration {
 
 
