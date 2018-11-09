0b75d9ad95b45c9e4e51f0d2b324c706c5f13c83
zhaixm <zxmsdyz@sina.com>
2018-11-09 23:55:17 +0800

    设置client自动配置文件的order，保证UserAuthInterceptor在最前面执行

--- client/src/main/java/com/jay/vito/uic/client/config/AutoConfiguration.java
+++ client/src/main/java/com/jay/vito/uic/client/config/AutoConfiguration.java
@@ -3,9 +3,11 @@ package com.jay.vito.uic.client.config;
 import com.jay.vito.uic.client.interceptor.UserAuthInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+@Order(5)
 @Configuration("authClientConfig")
 public class AutoConfiguration extends WebMvcConfigurerAdapter {
 
