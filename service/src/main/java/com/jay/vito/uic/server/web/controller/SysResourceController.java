be1422980179684f5ce06ae1801a299c4c1e78b2
zhaixm <zxmsdyz@sina.com>
2019-01-26 17:17:34 +0800

    根节点进行排序
    (cherry picked from commit 485008eb7a59f70769a4a4c6ae05cff3e22d9e38)

--- service/src/main/java/com/jay/vito/uic/server/web/controller/SysResourceController.java
+++ service/src/main/java/com/jay/vito/uic/server/web/controller/SysResourceController.java
@@ -13,10 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * 资源接口控制器
@@ -108,6 +105,7 @@ public class SysResourceController extends BaseGridCRUDController<SysResource, L
 				rootNodes.add(node);
 			}
 		});
+		Collections.sort(rootNodes, Comparator.comparing(ResourceNode::getSortNo));
 		return rootNodes;
 	}
 
